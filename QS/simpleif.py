8e239a58266b98d4c69ab99bedc13d094329e8f6
Yoshobanta <95220495+yoshobanta@users.noreply.github.com>
2025-06-07 13:14:15 +0530

    Today's QS Problems Normal Control statem and  Programs Releated to it

---
+++ QS/simpleif.py
@@ -0,0 +1,77 @@
+# WAP to check whether the entered number is positive or not.
+a = int(input('enter the number:'))
+if a > 0:
+    print('the entered number is positive number')
+print('the execution of a program is done')
+
+# WAP to check whether the entered number is even or not.
+a = int(input('enter the number:'))
+if a % 2 == 0:
+    print('the entered number is even number')
+else:
+    print('the entered number is odd number')
+    
+
+# WAP to check whether the entered number is divisible by 8 or not.
+a = int(input('enter the number:'))
+if a % 8 == 0:
+    print('the entered number is divisible by 8')
+else:
+    print('the entered numebr is not divisible by 8')
+
+# WAP to check whether the entered number is posotive or negative.
+a = int(input('enter the number:'))
+if a > 0 :
+    print('the entered number is positive')
+else:
+    print('the entered number is negative')
+    
+# WAP to check whether the entered character is upercase alphabet or not.
+a = input('enter the chatacter')
+if 'A' <= a <= 'Z':
+    print('the entered charcater is uppercase alphabet')
+else:
+    print('the entered character is not an uppercase alphabet')
+
+# WAP to find the greatest number among the given two integer numbers.
+a = int(input('enter the number 1:'))
+b = int(input('enter the number 2:'))
+if a > b:
+    print('a is the greatest number')
+elif b>a:
+    print('b is the greatest number')
+elif b == a:
+    print('both a & b are same')
+
+# WAP to find the greatest number among the given 3 integer numbers.
+a = int(input('enter the number 1:'))
+b = int(input('enter the number 2:'))
+c = int(input('enter the number 3:'))
+if a > b and a > c:
+    print(a)
+elif  b > c:
+    print(b)
+else:
+    print(c)
+
+# WAP to find the greatest number among the given 4 integer numbers.
+a = int(input('enter the number 1:'))
+b = int(input('enter the number 2:'))
+c = int(input('enter the number 3:'))
+d = int(input('enter the number 4:'))
+if a > b and a > c and a > d:
+    print(a)
+elif b > c and b > d:
+    print(b)
+elif c > d:
+    print(c)
+else:
+    print(d)
+    
+# WAP to check whether the entered character is lowercase alphabet or not.
+# WAP to check whether the entered character is numerical character or not.
+# WAP to check whether the entered character is special character or not.
+# WAP to find the greatest number among 5 numbers(without using">" operator)
+# WAP to find the type of a character.
+
+
