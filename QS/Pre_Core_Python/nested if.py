65eb268bc9cb26425272ed7ca1f46b0414b2c029
Yoshobanta <95220495+yoshobanta@users.noreply.github.com>
2025-06-09 12:58:01 +0530

    Todays class update .

---
+++ QS/Pre_Core_Python/nested if.py
@@ -0,0 +1,110 @@
+### WAP to check whether the entered character is vowel or consonent.
+##a = input('enter the charcter:')
+##if 'A' <= a <= 'Z' or 'a' <= a <= 'z':
+##    if a in 'AEIOUaeiou':
+##        print('the entered character is vowel character')
+##    else:
+##        print('the entered character is consonent')
+##else:
+##    print('the entered charcater is no at all an alphabet')
+
+### WAP to find the second greatest number among the given 3 integer numbers.
+##a = int(input('enter the number 1:'))
+##b = int(input('enter the number 2:'))
+##c = int(input('enter the number 3:'))
+##if a > b and a > c:
+##    if b > c:
+##        print(b)
+##    else:
+##        print(c)
+##elif b > c:
+##    if a > c:
+##        print(a)
+##    else:
+##        print(c)
+##else:
+##    if a > b:
+##        print(a)
+##    else:
+##        print(b)
+
+### WAP to find the second greatest number among the given 4 integer numbers.
+##a = int(input('enter the number 1:'))
+##b = int(input('enter the number 2:'))
+##c = int(input('enter the number 3:'))
+##d = int(input('enter the number 4:'))
+##if a > b and a > c and a > d:
+##    if b > c and b > d:
+##        print(b)
+##    elif c > d:
+##        print(c)
+##    else:
+##        print(d)
+##elif b > c and b > d:
+##    if a > c and a > d:
+##        print(a)
+##    elif c > d:
+##        print(c)
+##    else:
+##        print(d)
+##elif c > d:
+##    if a > b and a > d:
+##        print(a)
+##    elif b > d:
+##        print(b)
+##    else:
+##        print(d)
+##else:
+##    if a > b and a > c:
+##        print(a)
+##    elif b > c:
+##        print(b)
+##    else:
+##        print(c)
+
+### WAP to find the greatest number among the given 4 integer numbers.
+##a = int(input('enter the number 1:'))
+##b = int(input('enter the number 2:'))
+##c = int(input('enter the number 3:'))
+##d = int(input('enter the number 4:'))
+##
+##if a > b:
+##    if a > c:
+##        if a > d:
+##            print(a)
+##        else:
+##            print(d)
+##    else:
+##        if c > d:
+##            print(c)
+##        else:
+##            print(d)
+##else:
+##    if b > c:
+##        if b > d:
+##            print(b)
+##        else:
+##            print(d)
+##    else:
+##        if c > d:
+##            print(c)
+##        else:
+##            print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ QS/Pre_Core_Python/while loop.py
@@ -0,0 +1,43 @@
+# WAP to print "Hello World" for 5 time.
+i = 0
+while i < 5:
+    print('Hello World')
+    i += 1
+
+# WAP to print all the numbers from 1 to 100
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+# WAP to print the first n natural numbers.
+n = int(input('enter the number:'))
+i = 1
+while i <= n:
+    print(i)
+    i += 1
+
+# WAP to print all the numbers in between the user entered limits.
+s = int(input('enter the string:'))
+e = int(input('enter tje ending:'))
+i = s
+while i <= e:
+    print(i)
+    i = i + 1     # i += 1
+
+# WAP to print all the even numbers in between the user entered limits.
+s = int(input('enter the string:'))
+e = int(input('enter tje ending:'))
+i = s
+while i <= e:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1     # i += 1
+
+# WAP to print all the odd numbers in between the user entered limits
+# WAP to print all the numbers which are divisible by 7&9 in between the user entered limits
+
+
+
+
+
