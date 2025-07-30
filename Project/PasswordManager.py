7f24e5d40a28690d19ee935a79df7303bad57500
Yoshobanta <95220495+yoshobanta@users.noreply.github.com>
2025-07-30 21:10:31 +0530

    Added Encryption To the PassWord Manager

--- Project/PasswordManager.py
+++ Project/PasswordManager.py
@@ -1,39 +1,46 @@
-# Here we will store our password in a txt file.
-# We will ask the user if they want to add a new password or view the password .
-# We will also create a master password
-
 from cryptography.fernet import Fernet
 
+# One-time key generation
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
 
-master_pwd = input("What is the master password ?")
+# Load key from file
+def load_key():
+    with open("key.key", "rb") as key_file:
+        return key_file.read()
+
+# You only need to run this once to create the key
+# Uncomment the next line once to generate the key
+# write_key()
+
+key = load_key()
+fer = Fernet(key)
 
 def view():
-       with open("passwords.txt") as f:
-        for i in f:
-            
-            data = i.strip().split()             #For clean and no extra tralling or leading white space .
-            # data = i.split()
-            print(f"Account name - {data[0].strip()}")
-            print(f"Password - {data[2].strip()}")
+    with open("passwords.txt", "r") as f:
+        for line in f:
+            user, encrypted_pwd = line.strip().split(" | ")
+            decrypted_pwd = fer.decrypt(encrypted_pwd.encode()).decode()
+            print(f"Account name - {user}")
+            print(f"Password - {decrypted_pwd}")
             print("-------------------------------------------")
-        
 
 def add():
-    name = input("Account name : ")
-    pwd = input("Password : ")
-    with open("passwords.txt", "a") as f :
-        f.write(f"{name} | {pwd}\n")
+    name = input("Account name: ")
+    pwd = input("Password: ")
+    encrypted_pwd = fer.encrypt(pwd.encode()).decode()
+    with open("passwords.txt", "a") as f:
+        f.write(f"{name} | {encrypted_pwd}\n")
 
 while True:
-    mode = input("To add password type 'add' To view the password type - 'view' and to quit type - 'q'").lower()
-    if mode in ("quit","q"):
+    mode = input("To add password type 'add', to view passwords type 'view', or 'q' to quit: ").lower()
+    if mode in ("q", "quit"):
         break
-    
-    if mode in ("view","v"):
+    elif mode in ("view", "v"):
         view()
-    elif mode in ("add","a"):
+    elif mode in ("add", "a"):
         add()
     else:
-        print("Type a valid input")
-        continue
-        
+        print("Invalid input. Please type 'add', 'view', or 'q'.")
